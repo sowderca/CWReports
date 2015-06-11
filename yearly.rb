8ab7458c7ba1735b9f28f771a4d8e81656251315
Cameron Sowder <cameronsgit@gmail.com>
2015-06-11 14:27:29 -0400

    up to date

--- yearly.rb
+++ yearly.rb
@@ -5,32 +5,24 @@ require 'mailgun'
 require 'date'
 require 'time'
 require 'csv'
+require 'scruffy'
 #require 'to_csv'
 
 
 #Mailgun 
+=begin
 mg_client = Mailgun::Client.new("key-70f741f0b10b167ad20b09f841151af8")
 mb_obj = Mailgun::MessageBuilder.new
-
+=end
 def remove_file(file)
   File.delete(file)
 end
 
-#class Array
-#    def to_csv(csv_filename="hash.csv")
-#    CSV.open(csv_filename, "wb") do |csv|
-#        csv << first.keys
-#        self.each do |hash|
-#            csv << hash.value
-#        end
-#        end
-#    end
-#end
 =begin vars
 Set the dates for the report
 =end
 start_date = Date.parse("2014-04-01")
-end_date = Date.parse("2015-05-01")
+end_date = Date.parse("2015-05-27")
 comps = Array.new #array for new dates
 #gets only the weekdays from the start date untill the end date.
 days = start_date.business_dates_until(end_date)
@@ -45,7 +37,7 @@ comps.each do |day|
 	cw_client = ConnectStoopid::ReportingClient.new('connect.vc3.com', 'vc3', 'dattoCW', 'mushySh@pe83')
     
     @fcr_c = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_closed >= #{@morning} and date_closed < #{@evening} and (closed_by = 'sowderca' or closed_by = 'carterma' or closed_by = 'betivast' or closed_by = 'witherst' or closed_by = 'washingt' or closed_by = 'sleeperk' or closed_by = 'chandles' or closed_by = 'keddingl' or closed_by = 'williamm' or closed_by = 'lettswil' or closed_by = 'greencra' or closed_by = 'smitheli') and Board_Name = 'FCR'")
-	@infa_c = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_closed >= #{@morning} and date_closed < #{@evening} and (closed_by = 'sowderca' or closed_by = 'carterma' or closed_by = 'betivast' or closed_by = 'witherst' or closed_by = 'washingt' or closed_by = 'sleeperk' or closed_by = 'chandles' or closed_by = 'keddingl' or closed_by = 'williamm' or closed_by = 'lettswil' or closed_by = 'greencra' or closed_by = 'smitheli') and Board_Name = 'Infrastructure'")
+	@infa_c = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_closed >= #{@morning} and date_closed < #{@evening} and (closed_by = 'sowderca' or closed_by = 'carterma' or closed_by = 'betivast' or closed_by = 'witherst' or closed_by = 'washingt' or closed_by = 'sleeperk' or closed_by = 'chandles' or closed_by = 'keddingl' or closed_by = 'williamm' or closed_by = 'lettswil' or closed_by = 'greencra' or closed_by = 'smitheli') and Board_Name = 'Infrastructure Team'")
     @comm_c = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_closed >= #{@morning} and date_closed < #{@evening} and (closed_by = 'sowderca' or closed_by = 'carterma' or closed_by = 'betivast' or closed_by = 'witherst' or closed_by = 'washingt' or closed_by = 'sleeperk' or closed_by = 'chandles' or closed_by = 'keddingl' or closed_by = 'williamm' or closed_by = 'lettswil' or closed_by = 'greencra' or closed_by = 'smitheli') and (Board_Name = 'SC Comm Internal' or Board_Name  ='SC Comm Services')") 
     @sc_c = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_closed >= #{@morning} and date_closed < #{@evening} and (closed_by = 'sowderca' or closed_by = 'carterma' or closed_by = 'betivast' or closed_by = 'witherst' or closed_by = 'washingt' or closed_by = 'sleeperk' or closed_by = 'chandles' or closed_by = 'keddingl' or closed_by = 'williamm' or closed_by = 'lettswil' or closed_by = 'greencra' or closed_by = 'smitheli') and (Board_Name = 'SC Govt Internal' or Board_Name = 'SC Govt Internal')")
     @nc_c = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_closed >= #{@morning} and date_closed < #{@evening} and (closed_by = 'sowderca' or closed_by = 'carterma' or closed_by = 'betivast' or closed_by = 'witherst' or closed_by = 'washingt' or closed_by = 'sleeperk' or closed_by = 'chandles' or closed_by = 'keddingl' or closed_by = 'williamm' or closed_by = 'lettswil' or closed_by = 'greencra' or closed_by = 'smitheli') and (Board_Name = 'NC Govt Internal' or Board_Name = 'NC Govt Services')")
@@ -60,10 +52,10 @@ comps.each do |day|
     @lt_o = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_entered >= #{@morning} and date_entered < #{@evening} and Board_Name = 'Labtech Alerts - Internal' and (closed_by != 'ltadmin' or closed_by != 'Zadmin')")
     @atrees = cw_client.run_report_count("reportName" => "Service", "conditions" => "date_entered >= #{@morning} and date_entered < #{@evening} and Detail_Description contains 'Page ID'")
 
-@total_c = @fcr_c + @infa_c + @comm_c + @sc_c + @nc_c + @ga_c + @lt_c 
-@total_o = @fcr_o  + @comm_o + @sc_o + @nc_o + @ga_o 
+@total_c = @fcr_c + @infa_c + @comm_c + @sc_c + @nc_c + @ga_c + @lt_c rescue nil 
+@total_o = @fcr_o  + @comm_o + @sc_o + @nc_o + @ga_o  rescue nil
 
- hash = { :day => @morning, :total_c => @total_c, :fcr_c => @fcr_c, :infa_c => @infa_c, :comm_c => @comm_c, :sc_c => @sc_c, :nc_c => @nc_c, :ga_c => @ga_c, :lt_c => @lt_c, :total_o => @total_o, :fcr_o => @fcr_o, :infa_o => @infa_o, :comm_o => @comm_o, :sc_o => @sc_o, :nc_o => @nc_o, :ga_o => @ga_o, :lt_o => @lt_o,:atrees => @atrees}
+ hash = { :day => day, :total_c => @total_c, :fcr_c => @fcr_c, :infa_c => @infa_c, :comm_c => @comm_c, :sc_c => @sc_c, :nc_c => @nc_c, :ga_c => @ga_c, :lt_c => @lt_c, :total_o => @total_o, :fcr_o => @fcr_o, :infa_o => @infa_o, :comm_o => @comm_o, :sc_o => @sc_o, :nc_o => @nc_o, :ga_o => @ga_o, :lt_o => @lt_o,:atrees => @atrees}
 array.push(hash)
 end
 puts array
@@ -79,3 +71,29 @@ class Array
   end
 end
 array.to_csv("hash.csv")
+=begin
+#Email set up and send
+# Define the from address.
+mb_obj.set_from_address("reports@vc3.com", {"first"=>"Cameron", "last" => "Sowder"});
+# Define a to recipient.
+mb_obj.add_recipient(:to, "cameron.sowder@vc3.com", {"first" => "Cameron", "last" => "Sowder"});
+# Define a cc recipient.
+mb_obj.add_recipient(:to, "Amy.McKeown@vc3.com", {"first" => "Amy", "last" => "McKeown"});
+mb_obj.add_recipient(:to, "mark.carter@vc3.com", {"first" => "Mark", "last" => "Carter"});
+# Define the subject.
+mb_obj.set_subject("FCR  Report");
+# Define the body of the message.
+mb_obj.set_text_body("Report");
+# Set the Message-Id header. Pass in a valid Message-Id.
+mb_obj.set_message_id("<2014101400 0000.11111.11111@example.com>")
+# Clear the Message-Id header. Pass in nil or empty string.
+mb_obj.set_message_id(nil)
+mb_obj.set_message_id('')
+# Other Optional Parameters.
+mb_obj.add_attachment("Weekly.xlsx");
+
+
+# Send your message through the client
+mg_client.send_message("sandboxd511cc2c450b4b2d93f4bf5f4d385dae.mailgun.org", mb_obj)
+remove_file("hash.csv")
+=end
